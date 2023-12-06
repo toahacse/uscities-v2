d4d9c9eb1869321b6e5a302afbd53a04afd55743
toahacse <toahacse@gmail.com>
2023-12-06 20:06:44 +0600

    Us Cities server added done

---
+++ resources/views/vendor/toaha-admin/layouts/navigation.blade.php
@@ -0,0 +1,16 @@
+<div class="card card-sidebar-mobile">
+    <ul class="nav nav-sidebar" data-nav-type="accordion">
+
+        <li class="nav-item">
+            <a href="#" class="nav-link active text-light">
+                <i class="icon-home4"></i>
+                <span>
+                    Dashboard
+                </span>
+            </a>
+        </li>
+
+        @stack('master-sidebar')
+        
+    </ul>
+</div>
